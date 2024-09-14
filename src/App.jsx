47a27433b5c0a58f98e09acfe216e80a51f89ac9
vkartheekreddy <kartheekreddyvarimadugu@gmail.com>
2024-09-14 16:29:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import SearchBar from './components/SearchBar';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Country Search</h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
